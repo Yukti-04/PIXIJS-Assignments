1d25070b68778c2dd9ba6227db5b40cbcb814f7b
Yukti-04 <reman.arora0411@gmail.com>
2022-09-26 17:10:04 +0530

    PIXI Assignments

---
+++ Assignments/Assignment-6/src/index.ts
@@ -0,0 +1,15 @@
+import { Application } from 'pixi.js';
+import { Game } from './Game';
+import './css/main.css';
+window.onload = ()=>{
+    const gameDiv:HTMLDivElement = <HTMLDivElement>document.getElementById('game');
+    const app:Application = new Game({
+        resizeTo: gameDiv,
+        backgroundColor: 0x996633,
+        sharedLoader: true,
+        sharedTicker: true,
+        width: innerWidth,
+        height: innerHeight,
+    });
+    gameDiv.appendChild(app.view);
+}

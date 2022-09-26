9354f50c04eec38b3f693099a38691d7a0f18bcf
Yukti-04 <reman.arora0411@gmail.com>
2022-09-26 14:08:38 +0530

    Assignment-2

--- Assignments/Assignment-2/loader.mjs
+++ Assignments/Assignment-2/loader.mjs
@@ -1,47 +1,46 @@
 const game = new PIXI.Application({
-    width: 1900,
-    height: 1000,
-    // backgroundColor: 0x4D9111,
-    transparent: true,
-  
-  });
-  document.getElementById("game").append(game.view);
-  loadAssets([
-    { name: "back", url: "./Assets/coffee.png" },
-    { name: "front", url: "./Assets/coffeeBack.jpg" },
-  ], start);
-  //------------------------------
-  const pBar = document.getElementById("bar");
-  const pText = document.getElementById("progress");
-  
-  function preload(e) {
-    pBar.style.width = e.progress + "%";
-    pText.innerText = e.progress + "%";
-    // pText.style.textAlign = "center";
-    if (e.progress === 100) {
-      console.log("hide loader");
-      setTimeout(() => {
-        document.getElementById("loader").style.display = "none";
-      }, 2000);
-    }
-    console.log(e.progress);
+  width: 1900,
+  height: 1000,
+  // backgroundColor: 0x4D9111,
+  transparent: true,
+
+});
+document.getElementById("game").append(game.view);
+loadAssets([
+  { name: "back", url: "./Assets/coffeeback.jpeg" },
+  { name: "front", url: "./Assets/coffee.png" },
+], start);
+//------------------------------
+const pBar = document.getElementById("bar");
+const pText = document.getElementById("progress");
+
+function preload(e) {
+  pBar.style.width = e.progress+ "%";
+  pText.innerText = e.progress + "%";
+  // pText.style.textAlign = "center";
+  if (e.progress === 100) {
+    console.log("hide loader");
+    setTimeout(() => {
+      document.getElementById("loader").style.display = "none";
+    }, 2000);
   }
-  
-  function loadAssets(list, onLoadComplete) {
-    game.loader.onProgress.add(preload);
-    game.loader.add(list).load(onLoadComplete);
-  }
-  //----------------------------------
-  function start(loader, resources) {
-      console.log('params ', arguments);
-      const back = PIXI.Sprite.from(resources['back'].texture);
-      back.scale.set(0.2);
-      back.position.x=500;
-      game.stage.addChild(back);
-      const smily = new PIXI.Texture(resources['front'].texture,
-      new PIXI.Rectangle(0,0,100,100));
-      const front = PIXI.Sprite.from(smily);
-      front.width=300+"px";
-      front.scale.set(0.2);
-      game.stage.addChild(front);
-  }
+  console.log(e.progress);
+}
+
+function loadAssets(list, onLoadComplete) {
+  game.loader.onProgress.add(preload);
+  game.loader.add(list).load(onLoadComplete);
+}
+//----------------------------------
+function start(loader, resources) {
+    console.log('params ', arguments);
+    const back = PIXI.Sprite.from(resources['back'].texture);
+    back.scale.set(0.5);
+    back.position.x=500;
+    game.stage.addChild(back);
+    const smily = new PIXI.Texture(resources['front'].texture,
+    new PIXI.Rectangle(0,0,298,300));
+    const front = PIXI.Sprite.from(smily);
+    // front.scale.set(0.2);
+    game.stage.addChild(front);
+}
